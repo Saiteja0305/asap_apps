8b7721918da0b560662f2833f0f1892d5d0e5e64
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-27 09:44:32 +0000

    new changes

---
+++ asap_app/asap_app/src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = "PRODUCT_API/";
+
+const instance = axios.create({
+  baseURL
+});
+
+export const getTableData = async (params = { $top: 100, $skip: 0 }) => {
+  const { data } = await instance.get("/A_Product", {
+    params
+  });
+
+  return data.d?.results || data.d || data.value;
+};
+
+export const getTableCount = async () => {
+  const { data } = await instance.get("/A_Product/$count");
+  return data;
+};
