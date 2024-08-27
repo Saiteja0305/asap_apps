62f690974d899158f742e2b4a357f94660b52452
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-27 10:02:57 +0000

    new changes added

--- asap_app/asap_app/src/App.js
+++ asap_app/asap_app/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import NotFound from "pages/NotFound";
 import AppBar from "components/AppBar";
-import MasterPage from "pages/MasterPage";
 
 const App = () => {
   return (
     <div className="App full-height">
       <AppBar />
-      <MasterPage />
+      <h1>
+        Hello World
+        </h1>  
     </div>
   );
 };
